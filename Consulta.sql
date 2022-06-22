20556016b188c06f7475215a96e7a6e0385966d0
Igor Monardez <igormonardez@id.uff.br>
2022-06-21 22:39:27 -0300

    por Igor Monárdez: Exercicio 5 e 6

--- Consulta.sql
+++ Consulta.sql
@@ -17,9 +17,21 @@ WHERE F.id = P.id AND F.id = M.Professor_id AND N.Professor_id = M.Professor_id 
 GROUP BY F.id
 HAVING count(P.nome)>=3;
 
+-- Ex 5
+select Disciplina.nome,count(*) , count(distinct prof.id)
+from Professor prof, Disciplina, Turma_Ministrada tmin
+where tmin.professor_id = prof.id and
+		tmin.disciplina_id = Disciplina.id
+        group by disciplina_id;
+
+-- Ex 6
+select E.nome,P.nome
+from Escola E, Pessoa P, Cidade C
+where E.cidade_id = C.id AND P.id = E.diretor_id AND P.cidade_id != E.cidade_id;
+
 -- 7
 
-SELECT E.nome, COUNT(DISTINCT TM.turma_id), COUNT(DISTINCT TM.Professor_id)
+SELECT E.nome as Nome, COUNT(DISTINCT TM.turma_id) Turmas, COUNT(DISTINCT TM.Professor_id) Professores
 FROM Escola E JOIN Turma T ON E.id = T.escola_id
               JOIN Turma_Ministrada TM ON T.id = TM.turma_id
               JOIN Professor P ON TM.Professor_id = P.id
@@ -27,7 +39,7 @@ GROUP BY E.nome;
 
 -- 8
 
-SELECT E.nome, COUNT(DISTINCT A.id)/COUNT(DISTINCT TM.Professor_id) as 'Aluno/Professor'
+SELECT E.nome as Nome, COUNT(DISTINCT A.id)/COUNT(DISTINCT TM.Professor_id) 'Aluno/Professor'
 FROM Escola E JOIN Turma T ON E.id = T.escola_id
               JOIN Turma_Ministrada TM ON T.id = TM.turma_id
               JOIN Professor P ON TM.Professor_id = P.id
